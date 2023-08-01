f148f2bb41c01004252a069808ddebfbf7e0973b
+ <+>
2023-08-01 10:47:17 +0300

    updated components

---
+++ src/Restaurant.js/Login.js
@@ -0,0 +1,4 @@
+// // Sign Up as a new owner...should fill the details according to the data in the json
+
+// Customers  should also sign up/in
+// //     1. Outlet owners should be able to register their outlet in the system through the dashboard (optional)

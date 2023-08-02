b0e826ccfbbc8906d9ba2ba0bf4ca4fa933a0044
Bakhita87 <bakhitaotieno@gmail.com>
2023-08-03 02:04:50 +0300

    Initial message

---
+++ src/Customer/AddItemstoCart.js
@@ -0,0 +1,81 @@
+// Customers should add items to a cart and see the order summary before placing an order.
+import {useEffect, useState} from "react";
+import ItemsCard from "./ItemsCard";
+
+function AddItemstoCart(){
+    const [food,setFood] = useState([])
+    const [totalPrice,setTotalPrice] = useState(0)
+    const[cart,setCart] = useState([])
+
+    
+  useEffect(
+    ()=>{
+        fetch("http://localhost:3000/food")
+        .then(response => response.json())
+        .then((data) => {
+           setFood(data)
+           //setPrice([...price,data])
+           console.log(data)
+           console.log(food)
+          
+        })
+         
+    },  []
+  )
+
+  useEffect(
+    ()=>{
+        fetch("http://localhost:3000/cart")
+        .then(response => response.json())
+        .then((data) => {
+           setCart(data)
+           
+           
+          
+        })
+         
+    }, []
+  )
+    function handleSubmit(item){
+        const newPrice = totalPrice + item.price
+     setCart([...cart,item])
+     setTotalPrice(newPrice)
+    }
+
+    
+return(
+     <>
+     <h1>All Items</h1>
+     <section className="py-4 container">
+        <div className="row justify-content-center">
+           
+            {food.map(item=>(
+         <ItemsCard key ={item.id}  onSubmit={()=>handleSubmit(item)} item = {item}/>
+      ))}
+
+        </div>
+        <p>Cart Length {cart.length} </p>
+        <div>
+            {cart.map((item,index)=>(
+                <div key={index}>
+                    <p>{item.food } {item.price} </p> 
+                    <button onClick={()=>setCart()}>Remove</button>
+                    </div>
+                
+            ))}
+            <p>Total Price {cart
+                  .reduce(
+                    (accumulator, item) => accumulator + item.price,
+                    0
+                  )
+                  }</p>
+        </div>
+
+     </section> 
+     
+     </>   
+       
+    )
+}
+
+export default AddItemstoCart ;
